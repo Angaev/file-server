c3450bd60c377d73997c233278a8be6dee527cae
artem <artem.angaev@gmail.com>
2020-02-03 22:18:35 +0300

    add logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const multer = require('multer');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('This is uploader!');
+});
+
+app.use(express.static('src/public'));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
+const storageConfig = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "src/uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+app.use(multer({storage: storageConfig}).any());
+
+app.post('/upload', function (req, res, next) {
+   const fileData = req.files;
+   if (!fileData) {
+     res.send('File upload failed!');
+   } else {
+     res.send('File upload success!');
+   }
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Uploader listening on port 3000!');
 });
--- src/public/script.js
+++ src/public/script.js
@@ -3,7 +3,19 @@ jQuery(document).ready(function ($) {
     if ($(this).val() != '') $(this).prev().text('Выбрано файлов: ' + $(this)[0].files.length);
     else $(this).prev().text('Выберите файлы');
   });
-  $('.chous').click(function () {
-    // TODO send data
+  $('#upload').click(function () {
+    $(this).html('Загружаю.....');
+    const formData = new FormData();
+    for (let i = 0; i < $('.my')[0].files.length; i++) {
+      formData.append("file_"+i, $('.my')[0].files[i]);
+    }
+
+    const request = new XMLHttpRequest();
+
+    request.onload = function() {
+        $('#upload').html('Загрузка окончена');
+    };
+    request.open("POST", `http://127.0.0.1:3000/upload`);
+    request.send(formData);
   });
 });
